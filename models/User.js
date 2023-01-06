10a1e99230b3f05fca5c4b313638975ec816f72d
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-01-06 15:47:34 +0300

    get a user, update, delete are created

--- models/User.js
+++ models/User.js
@@ -40,6 +40,25 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    desc: {
+      type: String,
+      default: '',
+      max: 50,
+    },
+    city: {
+      type: String,
+      default: '',
+      max: 50,
+    },
+    from: {
+      type: String,
+       default: '',
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
   },
   { timestamps: true }
 );
